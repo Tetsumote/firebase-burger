33a15dfcb4789c16f1eed7546acd051531436640
Paweł Grajewski <unionproject88@gmail.com>
2020-09-17 00:04:33 +0200

    add global error chandler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Aux/Aux";
+import axios from "axios";
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+    componentDidMount() {
+      axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+      });
+      axios.interceptors.response.use(null, (error) => {
+        this.setState({ error: error });
+      });
+    }
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error}>{this.state.error.message}</Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
