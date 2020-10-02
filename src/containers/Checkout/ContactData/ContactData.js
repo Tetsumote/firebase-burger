103a0c43172ec21edaae972281aa12531f10586f
Paweł Grajewski <unionproject88@gmail.com>
2020-10-02 23:46:01 +0200

    add form input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,8 @@ import classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 
+import Input from "../../../components/UI/Input/Input";
+
 class ContactData extends Component {
   state = {
     name: "",
@@ -49,26 +51,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form action="">
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Your Name"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="email"
           name="email"
           placeholder="Your Email"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="street"
           placeholder="Street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="postal"
           placeholder="Postal Code"
