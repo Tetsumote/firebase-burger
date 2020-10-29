2870916f6c8ed072461831241742eb3548c8f5ec
Paweł Grajewski <unionproject88@gmail.com>
2020-10-29 23:54:53 +0100

    add order route with auth

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,7 +10,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders();
+    this.props.onFetchOrders(this.props.token);
   }
   render() {
     let orders = <Spinner />;
@@ -29,14 +29,15 @@ class Orders extends Component {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onFetchOrders: () => dispatch(actions.fetchOrders())
+    onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
   };
 };
 
 const mapStateToProps = (state) => {
   return {
     orders: state.order.orders,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   };
 };
 
