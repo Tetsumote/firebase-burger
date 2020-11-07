be236fae60a45da18e081b240f050ad4809ad810
Paweł Grajewski <unionproject88@gmail.com>
2020-11-07 23:01:28 +0100

    test update

--- src/components/Navigation/NavigationItems/NavigationItems.spec.js
+++ src/components/Navigation/NavigationItems/NavigationItems.spec.js
@@ -16,12 +16,12 @@ beforeEach(() => {
 
 describe("<NavigationItems />", () => {
   it("should render two <NavigationItem /> elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
 
   it("should render three <NavigationItem /> elements if authenticated", () => {
-    const wrapper = shallow(<NavigationItems isAuthenticated />);
+    //wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
